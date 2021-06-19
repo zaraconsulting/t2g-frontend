6644b24a4b98c87e61f9015947a0050f96b88c67
derekcodingtemple <derekh@codingtemple.com>
2021-06-19 13:30:22 -0500

    sidenav active class

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
+import { SideNavLink } from './SideNavLink';
 
 export const SideNav = () =>
 {
@@ -12,6 +13,10 @@ export const SideNav = () =>
         logout();
     }
 
+    const handleSelectLink = (l) => {
+        console.log(l);
+    }
+
 
     return (
         <div className="dashboard-nav bg-white rounded-lg shadow-xs">
@@ -21,12 +26,18 @@ export const SideNav = () =>
                     currentUser.loggedIn
                     ?
                     <React.Fragment>
-                            <li className="d-block rounded-lg active"><Link to="/"><i className="ti-search font-sm"></i><span> Search</span></Link></li>
+                        <SideNavLink data={{ route: '/', icon: 'search', text:'Search' }} />
+                        <SideNavLink data={{ route: '/postings', icon: 'pin2', text:'Postings' }} />
+                        <SideNavLink data={{ route: '/', icon: 'world', text:'Sports' }} />
+                        <SideNavLink data={{ route: '/profile', icon: 'user', text:'Profile' }} />
+                        <SideNavLink data={{ route: '/password', icon: 'lock', text:'Password' }} />
+                        <li className="d-block rounded-lg"><Link to="" onClick={(e) => handleLogout(e)}><i className="ti-power-off font-sm"></i><span> Logout</span></Link></li>
+                            
+                            {/* <li className="d-block rounded-lg active"><Link to="/"><i className="ti-search font-sm"></i><span> Search</span></Link></li>
                             <li className="d-block rounded-lg"><Link to="/postings"><i className="ti-pin2 font-sm"></i><span> Postings</span></Link></li>
                             <li className="d-block rounded-lg"><Link to="/"><i className="ti-world font-sm"></i><span> Sports</span></Link></li>
                             <li className="d-block rounded-lg"><a href="/profile"><i className="ti-user font-sm"></i><span> Profile</span></a></li>
-                            <li className="d-block rounded-lg "><Link to="/password"><i className="ti-lock font-sm"></i><span> Password</span></Link></li>
-                            <li className="d-block rounded-lg"><Link to="" onClick={(e) => handleLogout(e)}><i className="ti-power-off font-sm"></i><span> Logout</span></Link></li>
+                            <li className="d-block rounded-lg "><Link to="/password"><i className="ti-lock font-sm"></i><span> Password</span></Link></li> */}
                     </React.Fragment>
                     :
                     <li className="d-block rounded-lg active"><a href="." data-toggle="modal" data-target="#Modallogin"><i className="ti-download font-sm"></i><span> Login</span></a></li>
---
+++ src/components/SideNavLink.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+export const SideNavLink = (props) => {
+    const data = props.data
+    return (
+        <li className={`d-block rounded-lg ${ useLocation().pathname == data.route ? 'active' : '' }`}>
+            { data.route !== '/profile' ? <Link to={data.route}><i className={`ti-${ data.icon } font-sm`}></i><span> {data.text}</span></Link> : <a href={data.route}><i className={`ti-${ data.icon } font-sm`}></i><span> {data.text}</span></a> }
+        </li>
+    )
+}
